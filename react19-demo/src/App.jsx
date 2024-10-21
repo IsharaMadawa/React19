e7452b2a4d400f3dfb682ca6983ee18cbeb26331
IsharaKumararathna <136785516+IsharaKumararathna@users.noreply.github.com>
2024-10-21 07:52:11 +0530

    4 - init

--- react19-demo/src/App.jsx
+++ react19-demo/src/App.jsx
@@ -1,12 +1,19 @@
+import { Suspense } from "react";
 import './App.css'
-import TodoList from "./components/todo";
+// import Posts from "./components/api-fetch";
+import PostsReact19 from "./components/api-fetch-react-19";
 
 function App() {
 
   return (
-    <div>
-      <TodoList />
-    </div>
+    <Suspense fallback={<p>Loading...</p>}>
+      <div>
+        <h2>use() API</h2>
+
+        {/* <Posts /> */}
+        <PostsReact19 />
+      </div>
+    </Suspense>
   )
 }
 
